e8036e7f59006b5e622119db5120eed817848de4
kky8822 <99176588+kky8822@users.noreply.github.com>
2022-06-05 20:45:50 +0900

    220605_react_typescript_styledComp_theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
+import styled from "styled-components";
 
 function App() {
+  const Container = styled.div`
+    background-color: ${(props) => props.theme.bgColor};
+  `;
+  const H1 = styled.h1`
+    color: ${(props) => props.theme.textColor};
+  `;
+  const Btn = styled.button`
+    background-color: ${(props) => props.theme.btnColor};
+    color: ${(props) => props.theme.textColor};
+  `;
+
   return (
     <div>
-      <Circle borderColor={"black"} bgColor={"teal"} />
-      <Circle text="Hi" bgColor={"tomato"} />
+      <Container>
+        <H1>Hi</H1>
+        <Btn>Click</Btn>
+      </Container>
     </div>
   );
 }
